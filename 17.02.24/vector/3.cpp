5358b6dcbd99e28e3ecacd4b8d4d3f4dcf960cc6
Svetlana Khokholeva <57761906+Khokholeva@users.noreply.github.com>
2024-02-17 11:03:13 +0300

    Create 3.cpp

---
+++ 17.02.24/vector/3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+  
+	vector<double> w{1, 3,5,7,9};
+  double u[5];
+  copy(w.begin(), w.end(), u);
+  for (double x : u) {
+	  cout << x << " ";
+  }
+	cout << endl;
+	return EXIT_SUCCESS;
+}
