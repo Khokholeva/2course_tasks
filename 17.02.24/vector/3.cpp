b84a4efbf41e1268960f2a6f731817cb340aa869
Svetlana Khokholeva <57761906+Khokholeva@users.noreply.github.com>
2024-02-17 11:10:13 +0300

    Update 3.cpp

--- 17.02.24/vector/3.cpp
+++ 17.02.24/vector/3.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int main()
 {
-  setlocale(LC_ALL, "Russian");
-  vector<double> w{1, 3,5,7,9};
-  double u[5];
-  copy(w.begin(), w.end(), u);
-  for (double x : u) {
-	  cout << x << " ";
-  }
+	setlocale(LC_ALL, "Russian");
+	vector<double> w{1, 3,5,7,9};
+	double u[5];
+	copy(w.begin(), w.end(), u);
+	for (double x : u) {
+		cout << x << " ";
+	}
 	cout << endl;
 	return EXIT_SUCCESS;
 }
