1edc98166dccf9dc72ed7ae2b336fd979427d951
Svetlana Khokholeva <57761906+Khokholeva@users.noreply.github.com>
2024-02-24 11:00:39 +0300

    Update tasks.cpp

--- 17.02.24/string/tasks.cpp
+++ 17.02.24/string/tasks.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
 #include <string>
 using namespace std;
 
@@ -7,24 +9,22 @@ using namespace std;
 bool valid_ip(string s) {
 	int dot, prev = 0;
 	string a;
-	string test = "1234567890";
 	int n;
 	for (int i = 0; i < 3; i++) {
 		dot = s.find_first_of(".", prev);
-		if (dot == -1) return false;
+		if (dot - prev > 3 or dot - prev < 1) return false;
 		a = s.substr(prev, dot - prev);
-		if (a.size() > 3) return false;
 		for (int i = 0; i < a.size(); i++) {
-			if (test.find_first_of(a[i]) == -1) return false;
+			if (not isdigit(a[i])) return false;
 		}
 		n = stoi(a);
 		if (n < 0 or n > 255) return false;
 		prev = dot + 1;
 	}
-	a = s.substr(prev, dot - prev);
-	if (a.size() > 3) return false;
+	a = s.substr(prev);
+	if (a.size() < 1 or a.size() > 3) return false;
 	for (int i = 0; i < a.size(); i++) {
-		if (test.find_first_of(a[i]) == -1) return false;
+		if (not isdigit(a[i])) return false;
 	}
 	n = stoi(a);
 	if (n < 0 or n > 255) return false;
@@ -47,6 +47,43 @@ string bracket(string base, string pattern) {
 }
 
 //task 3
+string edit_sums(string base) {
+	int l = 0, r = 0, summ = 0, n = 0;
+	string ans, a;
+	int s = base.size();
+	for (int i = 0; i < s; i++) {
+		if (isdigit(base[i])) {
+			r++;
+		}
+		else if (base[i] == '+' and i != 0 and i != s-1 and isdigit(base[i-1]) and isdigit(base[i+1])) {
+			a = base.substr(l, r - l);
+			n = stoi(a);
+			summ += n;
+			r++;
+			l = r;
+			
+		}
+		else {
+			if (r > l) {
+				a = base.substr(l, r - l);
+				n = stoi(a);
+				summ += n;
+				ans += to_string(summ);
+				summ = 0;
+			}
+			ans += base[i];
+			r++;
+			l = r;
+		}
+	}
+	if (r > l) {
+		a = base.substr(l, r - l);
+		n = stoi(a);
+		summ += n;
+		ans += to_string(summ);
+	}
+	return ans;
+}
 
 int main()
 {
