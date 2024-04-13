baf1ec5f0f927e3c2c03c08eb2fe31cc794b5d3c
Svetlana Khokholeva <57761906+Khokholeva@users.noreply.github.com>
2024-04-13 12:51:02 +0300

    Update search_tree.cpp

--- 6.04.24/search_tree.cpp
+++ 6.04.24/search_tree.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 struct SNode {
 	int data, key;
 	SNode* left, * right;
-	SNode(int k, int d, SNode * l = nullptr, SNode * r = nullptr) : key(k), data(d), left(l), right(r) {}
+	SNode(int k, int d, SNode* l = nullptr, SNode* r = nullptr) : key(k), data(d), left(l), right(r) {}
 
-	};
+};
 
 void f_print(SNode* r, int d = 0)
 {
@@ -51,12 +52,50 @@ void f_insert(SNode*& r, int k, int d) {
 	else f_insert(r->right, k, d);
 }
 
+void f_delete(SNode*& r, int k) {
+	if (r == nullptr) return;
+	if (r->key == k) {
+		if (r->left == nullptr) {
+			SNode* n = r->right;
+			delete r;
+			r = n;
+			return;
+		}
+		if (r->right == nullptr) {
+			SNode* n = r->left;
+			delete r;
+			r = n;
+			return;
+		}
+		SNode* prev = nullptr;
+		SNode* n = r->right;
+		while (n->left != nullptr) { 
+			prev = n;
+			n = n->left;
+		}
+		r->key = n->key;
+		r->data = n->data;
+		if (prev == nullptr) r->right = n->right;
+		else prev->left = n->right;
+		delete n;
+		return;
+	}
+	if (r->key > k) return f_delete(r->left, k);
+	return f_delete(r->right, k);
+}
+
+bool f_check(SNode* r, int min_k=INT32_MIN, int max_k=INT32_MAX) { 
+	if (r == nullptr) return true;
+	if (r->data < min_k or r->data > max_k) return false;
+	return f_check(r->left, min_k, r->data - 1) and f_check(r->right, r->data + 1, max_k);
+}
+
 struct STree
 {
 	SNode* root;
-	STree() : root(nullptr){}
+	STree() : root(nullptr) {}
 	STree(SNode* p) : root(p) {}
-	STree(int k, int d, SNode* left = nullptr, SNode* right = nullptr) : root(new SNode(k, d,left, right)) {}
+	STree(int k, int d, SNode* left = nullptr, SNode* right = nullptr) : root(new SNode(k, d, left, right)) {}
 	~STree() {
 		f_del(root);
 	}
@@ -91,37 +130,84 @@ struct STree
 		}
 		*p = new SNode(k, d);
 	}
+	void delete_rec(int k) {
+		f_delete(root, k);
+	}
+	void delete_cyc(int k) {
+		SNode* r = root;
+		SNode* par = nullptr;
+		int side = 0;
+		while (r != nullptr) {
+			if (r->key == k) break;
+			par = r;
+			if (r->key < k) { r = r->right; side = 1; }
+			else if (r->key > k) { r = r->left; side = 0; }
+		}
+		if (r == nullptr) return;
+		if (r->left == nullptr) {
+			if (par != nullptr) {
+				if (side == 0) par->left = r->right;
+				else par->right = r->right;
+			}
+			else root = r->right;
+			delete r;
+			return;
+		}
+		if (r->right == nullptr) {
+			if (par != nullptr) {
+				if (side == 0) par->left = r->left;
+				else par->right = r->left;
+			}
+			else root = r->left;
+			delete r;
+			return;
+		}
+		SNode* n = r->right;
+		SNode* prev = nullptr;
+		while (n->left != nullptr) {
+			prev = n;
+			n = n->left;
+		}
+		r->key = n->key;
+		r->data = n->data;
+		if (prev == nullptr) r->right = n->right;
+		else prev->left = n->right;
+		delete n;
+		return;
+	}
+	bool check() {
+		return f_check(root, INT32_MIN, INT32_MAX);
+	}
 };
-
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	srand((unsigned)time(0));
-	int k;
-	STree tree1, tree2;
-	for (int i = 1; i < 11; i++) {
-		k = rand() % 100;
-		cout << k << ": " << i << endl;
-		tree1.insert_cyc(k, i);
-		tree2.insert_rec(k, i);
-	}
-	cout << endl;
-	cout << "cyc tree:" << endl;
-	tree1.print();
-	cout << endl;
-	cout << "rec tree:" << endl;
-	tree2.print();
-	cout << endl;
-	SNode* p;
-	for (int i = 0; i < 5; i++) {
-		cin >> k;
-		p = tree1.search_cyc(k);
-		if (p != nullptr) cout << "cyc found " <<p->data << endl;
-		else cout << "cyc: No key" << endl;
-		p = tree2.search_rec(k);
-		if (p != nullptr) cout << "rec found " << p->data << endl;
-		else cout << "rec: No key" << endl;
-	}
+	SNode* p12 = new SNode(12, 12),
+		* p10 = new SNode(10, 10),
+		* p11 = new SNode(11, 11, p10, p12),
+		* p9 = new SNode(9, 9, nullptr, p11),
+		* p4 = new SNode(4, 4),
+		* p3 = new SNode(3, 3, nullptr, p4),
+		* p6 = new SNode(6, 6),
+		* p5 = new SNode(5, 5, p3, p6),
+		* p7 = new SNode(7, 7, p5),
+		* p1 = new SNode(1, 1),
+		* p2 = new SNode(2, 2, p1, p7),
+		* p8 = new SNode(8, 8, p2, p9);
+	STree t(p8);
+	t.print();
+	cout << endl << endl;
+	t.delete_cyc(8);
+	t.delete_rec(2);
+	t.print();
+	cout << endl << endl;
+	cout << boolalpha << t.check() << endl;
+	p6->key = 8;
+	p6->data = 8; 
+	t.print();
+	cout << endl << endl;
+	cout << boolalpha << t.check() << endl;
+
 	return EXIT_SUCCESS;
 }
